a464636a42271f44341974772c6006258dd22312
Phong Tran <s2@puyonexus.com>
2018-08-03 15:26:27 -0400

    Additional screen regions, using placeholder values

--- calibrate_scrn.py
+++ calibrate_scrn.py
@@ -2,6 +2,7 @@ import os.path
 import mss
 from PIL import Image
 
+
 # Set monitor and screen regions
 directory = os.path.dirname(os.path.abspath(__file__))
 with mss.mss() as sct:
@@ -14,11 +15,25 @@ with mss.mss() as sct:
     # Set screen regions (Puyo Puyo Chronicle)
     Player1 = {
         'board': {'top': 115, 'left': 294, 'width': 432, 'height': 805},
-        'next': {'top': 115, 'left': 726, 'width': 100, 'height': 200}
+        'next': {'top': 115, 'left': 726, 'width': 100, 'height': 200},
+        'score': {'top': 930, 'left': 294, 'width': 432, 'height': 100}
+    }
+    Player1['next_bit'] = {
+        'top': int(round(Player1['next']['top'] + Player1['next']['height'] * 0.1, 0)),
+        'left': int(round(Player1['next']['left'] + Player1['next']['width'] * 0.3, 0)),
+        'width': int(round(Player1['next']['width'] * 0.4, 0)),
+        'height': int(round(Player1['next']['height'] * 0.1, 0))
     }
     Player2 = {
         'board': {'top': 115, 'left': 1194, 'width': 432, 'height': 805},
-        'next': {'top': 115, 'left': 1094, 'width': 100, 'height': 200}
+        'next': {'top': 115, 'left': 1094, 'width': 100, 'height': 200},
+        'score': {'top': 930, 'left': 1194, 'width': 432, 'height': 100}
+    }
+    Player2['next_bit'] = {
+        'top': int(round(Player2['next']['top'] + Player2['next']['height'] * 0.1, 0)),
+        'left': int(round(Player2['next']['left'] + Player2['next']['width'] * 0.3, 0)),
+        'width': int(round(Player2['next']['width'] * 0.5, 0)),
+        'height': int(round(Player2['next']['height'] * 0.1, 0))
     }
     print('Player 1 Board: ' + str(Player1['board']))
     print('Player 1 NEXT: ' + str(Player1['next']))
@@ -28,12 +43,9 @@ with mss.mss() as sct:
     # Outline game areas for debugging purposes
     outline = Image.open(directory + '/img/calibration/outline.png')
     outline2 = Image.open(directory + '/img/calibration/outline2.png')
-    # Player 1
     outline = outline.resize((Player1['board']['width'], Player1['board']['height']))
     monitor.paste(outline, (Player1['board']['left'], Player1['board']['top']),
                                 mask = outline)
-    # Player 2
-    outline = outline.resize((Player2['board']['width'], Player2['board']['height']))
     monitor.paste(outline, (Player2['board']['left'], Player2['board']['top']),
                                 mask = outline)
 
@@ -41,8 +53,22 @@ with mss.mss() as sct:
     outline2 = outline2.resize((Player1['next']['width'], Player1['next']['height']))
     monitor.paste(outline2, (Player1['next']['left'], Player1['next']['top']), mask=outline2)
     monitor.paste(outline2, (Player2['next']['left'], Player2['next']['top']), mask=outline2)
+    # Outline portion of NEXT windows used to infer placement timing.
+    outline2 = outline2.resize((Player1['next_bit']['width'], Player1['next_bit']['height']))
+    monitor.paste(outline2, (Player1['next_bit']['left'], Player1['next_bit']['top']), mask=outline2)
+    monitor.paste(outline2, (Player2['next_bit']['left'], Player2['next_bit']['top']), mask=outline2)
+
+    # Outline player scores. Maybe this'll get used later.
+    outline = Image.open(directory + '/img/calibration/outline.png')
+    # Player 1
+    outline = outline.resize((Player1['score']['width'], Player1['score']['height']))
+    monitor.paste(outline, (Player1['score']['left'], Player1['score']['top']),
+                                mask = outline)
+    monitor.paste(outline, (Player2['score']['left'], Player2['score']['top']),
+                                mask = outline)
 
     # Board cell size.
+    outline2 = Image.open(directory + '/img/calibration/outline2.png')
     cell_width = Player1['board']['width'] / 6
     cell_height = Player1['board']['height'] / 12
     print('Cell dimensions: ' + str((cell_width, cell_height)))  # Check for roundoff error
